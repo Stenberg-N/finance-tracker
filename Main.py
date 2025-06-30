5818eafaf543f82b4a39aec8292513cfaa100b48
Niko! <niko.stenberg@protonmail.com>
2025-06-30 16:24:36 +0300

    Removal of data function and filter transactions by month/year

--- Main.py
+++ Main.py
@@ -39,7 +39,7 @@ def insert_transaction():
     connect_to_database.commit()
     connect_to_database.close()
 
-def view_transaction():
+def view_all_transactions():
     connect_to_database = sqlite3.connect(db_path)
     db_cursor = connect_to_database.cursor()
     db_cursor.execute('SELECT * FROM transactions')
@@ -49,19 +49,55 @@ def view_transaction():
         print(row)
     connect_to_database.close()
 
+def view_transaction_by_month():
+    connect_to_database = sqlite3.connect(db_path)
+    db_cursor = connect_to_database.cursor()
+    month_year = input("Enter month and year (MM-YYYY): ")
+    search_pattern = f'%-{month_year}'
+    db_cursor.execute('SELECT * FROM transactions WHERE date LIKE ?', (search_pattern,))
+    rows = db_cursor.fetchall()
+    print(f"\n=== Transaction history for {month_year} ===")
+    if not rows:
+        print("No transactions found for this period.")
+    else:
+        for row in rows:
+            print(row)
+    connect_to_database.close()
+
+def clear_all_transactions():
+    connect_to_database = sqlite3.connect(db_path)
+    db_cursor = connect_to_database.cursor()
+    db_cursor.execute('DELETE FROM transactions')
+    connect_to_database.commit()
+    connect_to_database.execute('VACUUM')
+    connect_to_database.close()
+    print("All transactions have been deleted and the database file has been compacted.")
+
 init_db()
 
 while True:
     print("\n1. Add transaction")
     print("2. View transactions")
-    print("3. Exit")
+    print("3. View transactions by month")
+    print("4. Clear all transactions")
+    print("5. Exit")
     choice = input("Choose option: ")
 
     if choice == '1':
         insert_transaction()
     elif choice == '2':
-        view_transaction()
+        view_all_transactions()
     elif choice == '3':
+        view_transaction_by_month()
+    elif choice == '4':
+        print("Are you sure you want to delete all of your data?")
+        final_choice = input("If you are, input the letter 'y'. If not, input the letter 'n' and press enter: ")
+        if final_choice.lower() == 'y':
+            clear_all_transactions()
+        else:
+            print("Your data has not been removed.")
+            continue
+    elif choice == '5':
         break
     else:
         print("Invalid input.")
