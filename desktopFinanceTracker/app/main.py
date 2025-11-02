1a2e0310ce105bc26efc20a80a2b8b23e4fd00af
Niko! <niko.stenberg@protonmail.com>
2025-11-02 19:39:17 +0200

    Fix[DESKTOP]: Switch joblib's backend to threading to fix random forest model spawning multiple app instances

--- desktopFinanceTracker/app/main.py
+++ desktopFinanceTracker/app/main.py
@@ -1,8 +1,11 @@
 from database.db import initDB
 from app.application import Application
 from app.config import DB_PATH
+import matplotlib, multiprocessing
+matplotlib.use('TkAgg')
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     DB_PATH.parent.mkdir(exist_ok=True)
     initDB()
     app = Application()
